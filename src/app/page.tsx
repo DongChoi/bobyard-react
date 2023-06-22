790af50e871aa0e0721fc628a564c2798a312f29
DongChoi <dongandrewchoi@gmail.com>
2023-06-22 15:42:17 -0700

    fixes  bugs in app/pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,15 @@ const Home = () => {
         <section>
           <h1>Welcome back {session.user.name}!</h1>
           <h3>
-            Remember to go at your own pace and don't be critical of yourself!
+            Remember to go at your own pace and don&apos;t be critical of
+            yourself!
           </h3>
         </section>
       ) : (
         <section className=" w-full flex-center">
           <h1 className="">You have reached Productiver</h1>
           <span className="satoshi primary-orange">
-            {" "}
-            We will help increase your productivity{" "}
+            We will help increase your productivity
           </span>
           <Feed />
         </section>
